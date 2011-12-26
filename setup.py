3b3eca169043eecf5655d6916d4720215a06b454
fay <fay@shanbay.com>
2011-12-26 13:58:20 +0800

    add locale

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='http://github.com/lmorchard/django-badger',
     license='BSD',
     packages=['badger', 'badger.management', 'badger.management.commands', 'badger.migrations'],
-    package_data={'badger': ['fixtures/*', 'templates/badger/*.html', 'templates/badger/includes/*.html']},
+    package_data={'badger': ['fixtures/*','locale/*', 'templates/badger/*.html', 'templates/badger/includes/*.html']},
     include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
